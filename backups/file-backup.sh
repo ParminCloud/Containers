33fd471ec9d211a28b07fd2421c9f5e369ecc198
Muhammed Hussein Karimi <info@karimi.dev>
2024-11-19 13:48:23 +0330

    :children_crossing: Validate number of files in backup dir
    Signed-off-by: Muhammed Hussein Karimi <info@karimi.dev>

--- backups/file-backup.sh
+++ backups/file-backup.sh
@@ -9,6 +9,13 @@ if [ ! -d "$DATA_DIR" ]; then
 	exit 1
 fi
 
+FILES_COUNT=$(ls -A1 "$DATA_DIR" | wc -l)
+
+if (( "$FILES_COUNT" < "0" )); then
+	log "Nothing to backup.!"
+	exit 1
+fi
+
 BACKUP_OUT=/tmp/backup.tar.gz
 
 log "Creating GZipped Tar Archive of backups"

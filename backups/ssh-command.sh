eaac51771f4d58dccdc2f27461e15027b38bf317
Muhammed Hussein Karimi <info@karimi.dev>
2024-12-06 21:25:46 +0330

    :sparkles: SSH command output backup
    Signed-off-by: Muhammed Hussein Karimi <info@karimi.dev>

--- backups/ssh-command.sh
+++ backups/ssh-command.sh
@@ -12,8 +12,8 @@ if [ -z "$SSH_PORT" ]; then
 	log "You need to set the SSH_PORT environment variable."
 	exit 1
 fi
-if [ -z "$DATA_DIR" ]; then
-	log "You need to set the DATA_DIR environment variable."
+if [ -z "$SSH_COMMAND" ]; then
+	log "You need to set the SSH_COMMAND environment variable."
 	exit 1
 fi
 export SSH_AUTH="key-file"
@@ -38,22 +38,20 @@ chmod 700 "${HOME}/.ssh"
 ssh-keyscan -p "${SSH_PORT}" "${SSH_HOST}" >> "${HOME}/.ssh/known_hosts"
 chmod 644 "${HOME}/.ssh/known_hosts"
 
-BACKUP_OUT=/tmp/backup.tar.gz
+BACKUP_OUT=/tmp/backup.gz
 
-log "Creating GZipped Tar Archive of backups"
-
-REMOTE_COMMAND="tar -C \"$DATA_DIR\" --acls --xattrs -cpaf - ."
+log "Creating GZipped file of command output"
 
 if [ "${SSH_AUTH}" = *"key"* ]; then
 	ssh -n \
 		-p "$SSH_PORT" \
 		"$SSH_USER"@"$SSH_HOST" \
-		"$REMOTE_COMMAND" > "${BACKUP_OUT}"
+		"$REMOTE_COMMAND" | gzip -9 > "${BACKUP_OUT}"
 else
 	sshpass -p "$SSH_PASSWORD" \
 		ssh -n \
 		-p "$SSH_PORT" \
 		"$SSH_USER"@"$SSH_HOST" \
-		"$REMOTE_COMMAND" > "${BACKUP_OUT}"
+		"$REMOTE_COMMAND" | gzip -9 > "${BACKUP_OUT}"
 fi
 echo "${BACKUP_OUT}"
